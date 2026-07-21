8690b6391bfc24ba0bc51bd8b434bc49b773f017
HarutoHiroki <root@harutohiroki.com>
2026-07-21 05:11:33 -0500

    if lady gaga exists where's sir googoo

--- src/custom_status_screen.c
+++ src/custom_status_screen.c
@@ -10,6 +10,7 @@
 #include <zmk/display/status_screen.h>
 #include <zmk/display/widgets/layer_status.h>
 #include <zmk/display/widgets/battery_status.h>
+#include <zmk/display/widgets/output_status.h>
 
 #include "widgets/bongo_cat.h"
 
@@ -27,8 +28,20 @@ static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+static struct zmk_widget_output_status output_status_widget;
+#endif
+
 lv_obj_t *zmk_display_status_screen(void) {
     lv_obj_t *screen = lv_obj_create(NULL);
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
+    // Endpoint (USB/BLE) + selected BLE profile number, top-left.
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_set_style_text_font(zmk_widget_output_status_obj(&output_status_widget),
+                               lv_theme_get_font_small(screen), LV_PART_MAIN);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     // Battery top-right; the cat/label sit in the empty space directly below it.
